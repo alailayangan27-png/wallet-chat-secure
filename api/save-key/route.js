fa2dbb298378210cce736f5085f6fc27afbb0d2c
alailayangan27-png <alailayangan27@gmail.com>
2026-04-25 04:16:11 +0700

    Update route.js

--- api/save-key/route.js
+++ api/save-key/route.js
@@ -2,8 +2,6 @@ import { redis } from "../../../lib/db";
 
 export async function POST(req) {
   const { address, publicKey } = await req.json();
-
   await redis.set(`pub:${address}`, publicKey);
-
   return Response.json({ ok: true });
 }

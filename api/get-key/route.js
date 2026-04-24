9129219c676bea40b8304f2d89b94d07bd8ea2df
alailayangan27-png <alailayangan27@gmail.com>
2026-04-25 03:13:49 +0700

    Create route.js

---
+++ api/get-key/route.js
@@ -0,0 +1,10 @@
+import { redis } from "../../../lib/db";
+
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const address = searchParams.get("address");
+
+  const publicKey = await redis.get(`pub:${address}`);
+
+  return Response.json({ publicKey });
+}
